c8783dd27352877b4ae610f98c9f57d600389abf
Valentin Coppin <valimero.ilou@gmail.com>
2017-03-11 04:22:19 +0100

    add files

--- src/main.py
+++ src/main.py
@@ -1,1 +1,3 @@
 from afficher import afficher
+from partie import partie
+from root import root
---
+++ src/root.py
@@ -0,0 +1,17 @@
+class root(object):
+	"""docstring for root"""
+	joueurs = []
+	def __init__(self):
+		while(r != 1 || r != 2)
+			r = input("\
+				=====================PyManille==========\n\
+				1: Inscrire un joueur\n\
+				2: Lister les joueurs")
+		if(r == 1):
+			creerJoueur()
+		elif(r == 2):
+			listerJoueurs()
+
+	def listerJoueurs():
+		for joueur in joueurs:
+			print(joueur)
