f4a855c7abfdf22ecf2fa92b32a333e291d47e14
Joseph Petit <joseph.barouf@gmail.com>
2017-03-29 18:19:18 +0200

    Perfectionnement de la méthode whoBegin et création de la méthode joueCarte

--- Code/Classe_Tour.py
+++ Code/Classe_Tour.py
@@ -10,15 +10,25 @@
 #-------------------------------------------------------------------------------
 
 class Tour : 
-	"""Classe organisans le déroulement d'un tour"""
-	def __init__(self, dealer) : 
-		self.dealer = dealer
+	"""Classe organisant le déroulement d'un tour"""
+	def __init__(self, kijou,first_joueur, decount) : 
+		self.kijou = kijou
+		self.first_joueur = first_joueur
+		self.decount = decount
 
 	def whoBegin(self) :
-		if self.dealer == 1 : 
-			J1.joueCarte()
+		if self.first_joueur == 1 :
+			self.joueCarte(joueur = J1)
 		else : 
-			J2.joueCarte()
-	def joueCarte(self) : 
-		
+			self.joueCarte(joueur = J2)
+	def joueCarte(self, joueur) :
+		if self.decount == 0 :
+			joueur = Joueur() #on va chercher la main du joueur en question dans la classe joueur
+			joueur.afficherCarteMainJoueur(joueur = joueur)
+			joueur.afficherCarteShowJoueur(joueur = joueur)
+		#elif 0 < self.decount < 60 :
+
+
+
+
 
