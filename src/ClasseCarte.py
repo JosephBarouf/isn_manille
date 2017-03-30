7409fc0e3f53830dd785e9907cfb55d788db114e
Valentin Coppin <valimero.ilou@gmail.com>
2017-03-30 19:42:36 +0200

    changement dossier Code par src

--- src/ClasseCarte.py
+++
@@ -1,5 +0,0 @@
-class Carte :	
-	def __init__(self, valeur, couleur, nom) :
-		self.nom = nom
-		self.valeur = valeur
-		self.couleur = couleur
--- src/main.py
+++ src/main.py
@@ -1,18 +1,19 @@
-from root import root
+from root2 import root 
 
 jeu = root()
 print("=================PyManille=================\n")
 
 while True:
 	r = 0
-	while(r!=1 and r!=2):
+	while(r!=1 and r!=2 and r!=3) :
 			r = int(input("\
 1: Créer un compte\n\
-2: Lister les compte\n"))
+2: Lister les comptes\n\
+3: Start\n"))
 
-	if(r == 1):
+	if(r == 1) :
 		jeu.creerCompte()
-	elif(r == 2):
+	elif(r == 2) :
 		jeu.listerCompte()
-	elif (r == 3):
-		print ("Lancement du jeu")
+	elif (r == 3) :
+		jeu.start()
